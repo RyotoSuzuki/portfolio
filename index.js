9d0ea27b22f38b5711551f2f086d0b3b3c72a078
RyotoSuzuki <71628644+RyotoSuzuki@users.noreply.github.com>
2021-01-26 14:18:13 +0900

    文字の修正

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-function noScroll(event) {
-   event.preventDefault();
- }
-window.onload = () => {
-    // スクロールを禁止する関数
+// function noScroll(event) {
+//    event.preventDefault();
+//  }
+// window.onload = () => {
+//     // スクロールを禁止する関数
     
-   // スクロール禁止(SP)
-   document.addEventListener('touchmove', noScroll, { passive: false });
-   // スクロール禁止(PC){ passive: false }
+//    // スクロール禁止(SP)
+//    document.addEventListener('touchmove', noScroll, { passive: false });
+//    // スクロール禁止(PC){ passive: false }
  
-   document.addEventListener('mousewheel', noScroll, { passive: false });
-   // スクロール禁止を解除(SP)
-//    document.removeEventListener('touchmove', noScroll, { passive: false });
-   // スクロール禁止を解除(PC)
-//    document.removeEventListener('mousewheel', noScroll, { passive: false });
- };
+//    document.addEventListener('mousewheel', noScroll, { passive: false });
+//    // スクロール禁止を解除(SP)
+// //    document.removeEventListener('touchmove', noScroll, { passive: false });
+//    // スクロール禁止を解除(PC)
+// //    document.removeEventListener('mousewheel', noScroll, { passive: false });
+//  };
 function buttonClick(){
-   removeEvent()
+   // removeEvent()
    scroll()
 }
 
-function removeEvent(){
+// function removeEvent(){
    
-   document.removeEventListener('touchmove', noScroll, { passive: false });
-   document.removeEventListener('mousewheel', noScroll, { passive: false });
-}
+//    document.removeEventListener('touchmove', noScroll, { passive: false });
+//    document.removeEventListener('mousewheel', noScroll, { passive: false });
+// }
 const div = document.querySelector('#title')
 div.animate({
   opacity: [0, 1]
